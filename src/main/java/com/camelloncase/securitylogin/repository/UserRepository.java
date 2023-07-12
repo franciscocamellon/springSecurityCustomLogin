30116e5162f71e3d8f4f74d17de0ce2c403dfb93
franciscocamellon <franciscocamellon@gmail.com>
2023-07-12 07:27:41 -0300

    Delete the unnecessary annotation @Repository

--- src/main/java/com/camelloncase/securitylogin/repository/UserRepository.java
+++ src/main/java/com/camelloncase/securitylogin/repository/UserRepository.java
@@ -1,12 +1,8 @@
 package com.camelloncase.securitylogin.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import com.camelloncase.securitylogin.model.User;
 
-
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 	User findByUsername (String username);
